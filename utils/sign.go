10a0bea31c0f9fadeb241e512b5431231ed0b1e7
HatsuneMona <songmiao39@foxmail.com>
2022-10-25 22:00:04 +0800

    feat (utils): 工具函数 构造待签名的字符串

---
+++ utils/sign.go
@@ -0,0 +1,69 @@
+package utils
+
+import (
+	"fmt"
+	"math/rand"
+	"net/url"
+	"sort"
+	"strings"
+	"time"
+)
+
+// GetRandomNonce 获取一个长度为 length 的随机字符串
+func GetRandomNonce(length int) string {
+	base := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+	result := make([]byte, length)
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < length; i++ {
+		index := r.Intn(len(base))
+		result[i] = base[index]
+	}
+	return string(result)
+}
+
+// GetUTCTimestamp 获取当前UTC时间戳
+func GetUTCTimestamp() string {
+	return time.Now().UTC().Format("2006-01-02T15:04:05Z")
+}
+
+func buildSignStr(paramMap map[string]string) string {
+	sortKey := make([]string, 0, len(paramMap))
+	for key, value := range paramMap {
+		if key != "" && value != "" {
+			sortKey = append(sortKey, key)
+		}
+	}
+
+	sort.Strings(sortKey)
+
+	signStr := "GET&" + percentEncode("/")
+
+	for _, key := range sortKey {
+		signStr = fmt.Sprint(signStr, "&", key, "=", percentEncode(paramMap[key]))
+	}
+
+	return signStr
+}
+
+// percentEncode 参数编码
+// 在阿里云 OpenAPI 调用中，我们需要对请求参数和请求值，使用 UTF-8 字符集按照RFC3986规则进行编码。具体编码规则如下：
+// - 字符 A~Z、a~z、0~9 以及字符-、_、.、~不编码。
+// - 对其它 ASCII 码字符进行编码。编码格式为%加上16进制的 ASCII 码。例如半角双引号（"）将被编码为 %22。
+// - 非 ASCII 码通过 UTF-8 编码。
+// - 空格编码成%20，而不是加号（+）。
+func percentEncode(src string) string {
+	t := []rune(src)
+	str := make([]rune, 0, len(t))
+
+	for _, char := range t {
+		switch char {
+		case '+', ' ':
+			str = append(str, []rune(" 20")...)
+		default:
+			str = append(str, char)
+		}
+	}
+
+	dst := url.QueryEscape(string(str))
+	return strings.ReplaceAll(dst, "+", "%")
+}
---
+++ utils/sign_test.go
@@ -0,0 +1,141 @@
+package utils
+
+import (
+	"testing"
+	"time"
+)
+
+func Test_getRandomNonce(t *testing.T) {
+	t.Run("test 5 times", func(t *testing.T) {
+		for i := 0; i < 5; i++ {
+			time.Sleep(time.Duration(1) * 321 * time.Nanosecond)
+			got := GetRandomNonce(i * 5)
+			t.Logf("%v time, getRandomNonce(lenth:%v) result: %s", i, i*5, got)
+		}
+	})
+}
+
+func Test_getUTCTimestamp(t *testing.T) {
+	t.Run("test 5 times", func(t *testing.T) {
+		for i := 0; i < 5; i++ {
+			got := GetUTCTimestamp()
+			t.Logf("%v time, getUTCTimestamp() result: %s", i, got)
+			time.Sleep(time.Duration(1) * time.Second)
+		}
+	})
+}
+
+func Test_percentEncode(t *testing.T) {
+	// testStr := map[string]string{
+	// 	"不编码字符": "abc123DEF-_._~",
+	// 	"其它字符编码成%XY的格式，其中XY是字符对应ASCII码的16进制": `""{}[]:;<>,/?=!@#$%^&` + "`",
+	// 	"中文": "你好",
+	// 	"扩展的UTF-8字符 emoji 【😊    😘    😆】": "😊    😘    😆",
+	// 	"空格【 】（预期【%20】）":                 " ",
+	// 	"加号【+】（预期【%20】）":                 "+",
+	// 	"星号【*】（预期【%2A】）":                 "*",
+	// 	"波浪【~】（预期【~】）":                   "~",
+	// }
+	// for msg, str := range testStr {
+	// 	fmt.Printf("测试项 %s 的结果为【%s】\n", msg, url.QueryEscape(str))
+	// }
+
+	type args struct {
+		src string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			"不编码字符",
+			args{"abc123DEF-_._~"},
+			"abc123DEF-_._~",
+		},
+		{
+			"其它字符编码成%XY的格式，其中XY是字符对应ASCII码的16进制",
+			args{`""{}[]:;<>,/?=!@#$%^&` + "`"},
+			"%22%22%7B%7D%5B%5D%3A%3B%3C%3E%2C%2F%3F%3D%21%40%23%24%25%5E%26%60",
+		},
+		{
+			"中文",
+			args{"你好"},
+			"%E4%BD%A0%E5%A5%BD",
+		},
+		{
+			"扩展的UTF-8字符 emoji 【😆】",
+			args{"😆"},
+			"%F0%9F%98%86",
+		},
+		{
+			"空格【 】",
+			args{" "},
+			"%20",
+		},
+		{
+			"加号【+】",
+			args{"+"},
+			"%20",
+		},
+		{
+			"星号【*】",
+			args{"*"},
+			"%2A",
+		},
+		{
+			"波浪【~】",
+			args{"~"},
+			"~",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := percentEncode(tt.args.src); got != tt.want {
+				t.Errorf("percentEncode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_buildSignStr(t *testing.T) {
+	type args struct {
+		paramMap map[string]string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			"empty args",
+			args{map[string]string{}},
+			"GET&%2F",
+		},
+		{
+			"sort args",
+			args{map[string]string{
+				"C": "apple",
+				"A": "apple",
+				"B": "apple",
+			}},
+			"GET&%2F&A=apple&B=apple&C=apple",
+		},
+		{
+			"empty key or value",
+			args{map[string]string{
+				"C": "apple",
+				"":  "apple",
+				"B": "",
+			}},
+			"GET&%2F&C=apple",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := buildSignStr(tt.args.paramMap); got != tt.want {
+				t.Errorf("buildSignStr() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
